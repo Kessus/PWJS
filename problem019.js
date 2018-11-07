e698b7935df5299a260d95fcc354afd63bce4174
Kessus <grafiszek@wp.pl>
2018-11-07 13:50:50 +0100

    Poprawka do zadania 19

--- problem019.js
+++ problem019.js
@@ -1,13 +1,15 @@
-function carry(a) {
-    var arg = [];
-    return embed(1, arg, a);
-}
-
-function embed(iteration, arg, func) {
-    return (x) => { 
-      arg.push(x);
-      if( iteration == func.length )
-        return func(...arg);
-      return embed(iteration+1, arg, func);
-    };
+function carry(a){
+  var args = [];
+  if(arguments.length == 1){
+      return carry(a, args);
+  }
+  else{
+     return (x) => {
+       args = arguments[1];
+       args.push(x);
+       if(a.length == args.length)
+        return a(...args);
+     return carry(a, args);
+    }
+  }
 }
